0d30c9e320c33e8bcfe368541d904bc001a8458c
jaan18 <jovas28@hotmail.com>
2021-10-01 18:31:44 -0500

    new links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,10 @@ export const Footer = () => {
     <section id="footer" class="p-3 bg-success">
       <ul class="nav text-center justify-content-center flex-column flex-sm-row">
         <li class="nav-item">
-          <a href="/index.html" class="nav-link text-white">
+          <a
+            href="https://github.com/jaan18/rick-morty-api"
+            class="nav-link text-white"
+          >
             Github Code
           </a>
         </li>
@@ -13,7 +16,8 @@ export const Footer = () => {
       <div class="row">
         <div class="col-md-12 copy">
           <p class="text-center text-white">
-            Made by Jovani Almaraz - Rick and Morty API.
+            Made by Jovani Almaraz{" "}
+            <a href="https://rickandmortyapi.com/">- Rick and Morty API.</a>
           </p>
         </div>
       </div>
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -8,14 +8,14 @@ export const Showcase = () => {
           <h3 class="display-5 mb-0 mt-5">Rick and Morty Carousel</h3>
           <h1 class="display-1">Made with Reactjs</h1>
           <p class="lead d-none d-md-block">¡See the code here!.</p>
-          <button
-            id="book"
-            class="btn btn-lg btn-success px-5"
-            data-toggle="modal"
-            data-target="#bookingModal"
+
+          <a
+            href="https://github.com/jaan18/rick-morty-api"
+            class="btn btn-success  btn-lg btn-success px-5"
+            role="button"
           >
             ¡GITHUB REPOSITORY!
-          </button>
+          </a>
         </div>
       </div>
     </section>
